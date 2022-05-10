59a9880f8b2adf85da2fdb1155cd0c66428181da
Vahid V <vhid.vz@gmail.com>
2022-05-11 00:32:12 +0430

    updated data.py

--- data.py
+++ data.py
@@ -1,10 +1,100 @@
 import glob
 import math
+import random
 
 import numpy as np
 import tensorflow as tf
 
 from tensorflow import keras
+
+
+def feature(type: "byte" or "float" or "int", key, value):
+    if type == "byte":
+        return {key: tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))}
+    elif type == "float":
+        return {key: tf.train.Feature(float_list=tf.train.FloatList(value=[value]))}
+    elif type == "int":
+        return {key: tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))}
+
+
+def example(*features):
+    feature = {}
+    for key, value in features.items():
+        feature.update({key: value})
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+def sorted_labels(directory):
+    labels = glob.glob(directory + "/*")
+    labels.sort()
+    return labels
+
+
+def get_class_num(class_name, labels):
+    for i, label in enumerate(labels):
+        if class_name in label:
+            return i + 1  # 1-indexed
+
+
+def twisted_pair(pattern, size=None):
+    files = glob.glob(pattern)
+    random.shuffle(files)
+
+    if not size:
+        size = len(files)
+
+    result = []
+    for i in range(size):
+        while True:
+            file = random.choice(files)
+            if not files[i] == file:
+                result.append((files[i], file))
+                break
+
+    return result
+
+
+def convert_twisted_pair(pairs, labels):
+    result = []
+    for pair in pairs:
+        pool = []
+        for item in pair:
+            file_path = item
+            class_name = file_path.split("/")[-1]
+            class_label = get_class_num(class_name, labels)
+            pool.append((file_path, class_label))
+        result.append(pool)
+    return result
+
+
+def converted_twisted_pair_to_example(pairs):
+    for pair in pairs:
+        result = []
+        for i in range(len(pair)):
+            file_path, class_label = pair[i]
+            result.append(feature("byte", "image"+str(i+1),
+                          tf.io.decode_image(file_path)))
+            result.append(feature("int", "label"+str(i+1), class_label))
+        yield example(*result)
+
+
+def converted_twisted_pair_to_tfrecord(ds_example, filename, size=None):
+    if not size:
+        with tf.io.TFRecordWriter(filename + '.tfrecord') as tf_writer:
+            for example in ds_example:
+                tf_writer.write(example.SerializeToString())
+    else:
+        i = 0
+        count = 0
+        tf_writer = tf.io.TFRecordWriter(filename + '_' + str(i) + '.tfrecord')
+        for example in ds_example:
+            tf_writer.write(example.SerializeToString())
+            count += 1
+            if count == size:
+                i += 1
+                count = 0
+                tf_writer = tf.io.TFRecordWriter(
+                    filename + '_' + str(i) + '.tfrecord')
 
 
 def load_imagenet_dataset(directory, image_size=(224, 224), batch_size=32, mode: "tf" or "torch" = "tf", **kwargs):
